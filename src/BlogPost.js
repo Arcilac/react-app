b31674b6d8b01e3fac6b0a396ac225ac65d059e1
arcilac <arcilacamila08@gmail.com>
2024-05-09 11:20:37 -0500

    useNavigate

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,15 +1,21 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import { blogdata } from "./blogdata"
 
 function BlogPost() {
+    const navigate = useNavigate()
     const { slug } = useParams()
 
     const blogpost = blogdata.find(post => post.slug === slug)
+
+    const returnToBlog = () => {
+        navigate("/blog")
+    }
+
     return (
         <>
         <h2>{blogpost.title}</h2>
+        <button onClick={returnToBlog}>Return</button>
         <p>{blogpost.content}</p>
         <p>{blogpost.author}</p>
         
