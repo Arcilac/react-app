39a93886aed90490cba73055fcdf90b7ba4ef742
arcilac <arcilacamila08@gmail.com>
2024-05-09 10:41:12 -0500

    dynamic routes

---
+++ src/BlogPost.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import { blogdata } from "./blogdata"
+
+function BlogPost() {
+    const { slug } = useParams()
+
+    const blogpost = blogdata.find(post => post.slug === slug)
+    return (
+        <>
+        <h2>{blogpost.title}</h2>
+        <p>{blogpost.content}</p>
+        <p>{blogpost.author}</p>
+        
+        </>
+    )
+}
+
+export { BlogPost }
---
+++ src/blogdata.js
@@ -0,0 +1,19 @@
+const blogdata = []
+blogdata.push({ 
+  title: "What is React?",
+  slug: "what-is-react",
+  content: "React is a front-end library developed",
+  author: "Camila",
+})
+blogdata.push({ 
+  title: "What is JSX?",
+  slug: "what-is-jsx",
+  content: "JSX is a syntax extension for JavaScript",
+})
+blogdata.push({ 
+  title: "What is Babel?",
+  slug: "what-is-babel",
+  content: "Babel is a JavaScript compiler",
+})
+
+export { blogdata }
