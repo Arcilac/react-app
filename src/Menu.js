81769b2c653ece8cb074419480b60bae3414ce9b
arcilac <arcilacamila08@gmail.com>
2024-05-09 21:24:48 -0500

    public-private

--- src/Menu.js
+++ src/Menu.js
@@ -1,23 +1,32 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import { useAuth } from "./auth"
 
 function Menu() {
+  const auth = useAuth()
+
+ 
   return (
     <nav>
       <ul>
-        {routes.map(route => (
-          <li key={route.to}>
-            <NavLink
-            style={({ isActive }) => ({
-              color: isActive ? "red" : "blue",
-            })}
-            to={route.to}
-            >
-              {route.Text}
-            </NavLink>
-          </li>
-        ))}
-        
+        {routes.map(route => {
+          if (route.publicOnly && auth.user) 
+            return null 
+          if (route.private && !auth.user) 
+            return null 
+
+          return (
+            <li key={route.to}>
+              <NavLink 
+              style={({ isActive }) => ({
+                color: isActive ? "red" : "blue",
+              })}
+              to={route.to} >
+                {route.Text}
+              </NavLink>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
@@ -26,22 +35,28 @@ const routes = []
 routes.push({ 
   to: "/",
   Text: "Home",
+  private: false,
  })
 routes.push({ 
   to: "/blog",
   Text: "Blog",
+  private: false,
  })
 routes.push({ 
   to: "/profile",
   Text: "Profile",
+  private: true,
  })
 routes.push({ 
   to: "/login",
   Text: "Login",
+  private: false,
+  publicOnly: true,
  })
 routes.push({ 
   to: "/logout",
   Text: "Logout",
+  private: true,
  })
 
 export { Menu }
